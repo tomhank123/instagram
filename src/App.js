6e59ef7495f64ab4ae0cd7fd1abf6fa074fff011
Minh Duc <ductm@devblock.net>
2021-04-28 11:51:29 +0700

    feat: create routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import * as ROUTES from './constants/routes';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App"></div>
+    <Router>
+      <Switch>
+        <Route path={ROUTES.LOGIN}>LOGIN Page</Route>
+        <Route path={ROUTES.SIGN_UP}>SIGN_UP Page</Route>
+        <Route path={ROUTES.PROFILE}>PROFILE Page</Route>
+        <Route path={ROUTES.DASHBOARD} exact>DASHBOARD Page</Route>
+        <Route>Notfound Page</Route>
+      </Switch>
+    </Router>
   );
 }
 
