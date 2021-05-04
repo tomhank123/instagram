56f6285676e9a61755ae4233af845ed9a9d64eda
Minh Duc <ductm@devblock.net>
2021-05-04 17:40:58 +0700

    feat: create header and notfound page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { lazy, Suspense } from 'react';
 import * as ROUTES from './constants/routes';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import UserContext from './context/user';
+import useAuthListener from './hooks/use-auth-listener';
 
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
+const NotFound = lazy(() => import('./pages/not-found'));
 
 function App() {
+  const { user } = useAuthListener();
+  
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
