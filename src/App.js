649d74a3ad1269e81dfdce208a7117f4fc0fa1e0
Minh Duc <ductm@devblock.net>
2021-05-04 18:17:18 +0700

    feat: create timeline and post component

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import UserContext from './context/user';
 import useAuthListener from './hooks/use-auth-listener';
 
+import ProtectedRoute from './helpers/protected-route';
+
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
 const NotFound = lazy(() => import('./pages/not-found'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 
 function App() {
   const { user } = useAuthListener();
@@ -18,6 +21,9 @@ function App() {
           <Switch>
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+              <Dashboard />
+            </ProtectedRoute>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
--- src/hooks/use-photos.js
+++ src/hooks/use-photos.js
@@ -16,6 +16,7 @@ export default function usePhotos(user) {
     }
 
     getTimelinePhotos();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user?.userId]);
 
   return { photos };
